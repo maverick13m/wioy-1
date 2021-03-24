b1f00ff04474993dadff9d84628a9ffe59773723
maverick13m <maverickgoa@gmail.com>
2021-03-24 15:26:49 +0530

    lk,

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import TransactionScreen from './screens/Booktransaction'
 import SearchScreen from './screens/Searchscreen'
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
 
 import {createAppContainer} from 'react-navigation'
 import {createBottomTabNavigator} from 'react-navigation-tabs'
@@ -11,13 +11,30 @@ export default class App extends React.Component{
   render(){
     return (
       <View style={styles.container}>
-          <AppContainer/>
+        <AppContainer/>
       </View>
     );
   }
 }
-const TabNavigator = createBottomTabNavigator({Transaction:{screen:TransactionScreen},Search:{screen:SearchScreen}})
+const TabNavigator = createBottomTabNavigator({                                                                                                                                            
+  Transaction:{screen:TransactionScreen},
+  Search:{screen:SearchScreen},
+  defaultNavigationOptions: ({navigation})=>({
+    tabBatIcon:({})=>{
+      const routeName= navigation.state.routeName
+      if(routeName ==="Transaction"){
+        return(<Image/>)
+      }
+      else if(routeName ==="Search"){
+        return(<Image/>)
+      }
+    }
+  })
+})
+
+
 const AppContainer = createAppContainer(TabNavigator)
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
