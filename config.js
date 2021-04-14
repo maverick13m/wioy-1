04564a4d4fdecf6a0764a625dcf1844a2550f8fb
maverick13m <maverickgoa@gmail.com>
2021-04-14 15:21:36 +0530

    7kut ygb

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-import * as firebase from 'firebase';
+import * as firebase from 'firebase'
 require("@firebase/firestore")
 const firebaseConfig = {
     apiKey: "AIzaSyDicfJk_2a0oWiY6-JkzL0Bp_CXmeijNyQ",
@@ -9,7 +9,7 @@ const firebaseConfig = {
     messagingSenderId: "62265965647",
     appId: "1:62265965647:web:3cd7ddad4087df34923e3a"
   };
-  if(!firebase.apps.length){  
-    firebase.intializeApp(firebaseConfig)
-  }
-  export default firebase.firestore()
+  //if(!firebase.apps.length){  
+    firebase.initializeApp(firebaseConfig)
+  //}
+  export default firebase.firestore();
