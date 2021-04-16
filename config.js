247b70851d626d7a31a54633805b266ede6e6f89
maverick13m <maverickgoa@gmail.com>
2021-04-16 14:06:53 +0530

    Co-authored-by: neetikatari <neetikatari@users.noreply.github.com>

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ require("@firebase/firestore")
 const firebaseConfig = {
     apiKey: "AIzaSyDicfJk_2a0oWiY6-JkzL0Bp_CXmeijNyQ",
     authDomain: "wily-53ab3.firebaseapp.com",
-    databaseURL:"https://wily-53ab3.firebaseapp.com",
+    databaseURL:"https://wily-53ab3.firebaseio.com",
     projectId: "wily-53ab3",
     storageBucket: "wily-53ab3.appspot.com",
     messagingSenderId: "62265965647",
     appId: "1:62265965647:web:3cd7ddad4087df34923e3a"
   };
   //if(!firebase.apps.length){  
-    firebase.initializeApp(firebaseConfig)
+    firebase.initializeApp(firebaseConfig);
   //}
   export default firebase.firestore();
